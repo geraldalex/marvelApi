0abab89296afa98676a0f769e2c8d83c560e9ae3
Александр <41731792+geraldalex@users.noreply.github.com>
2022-07-05 16:10:54 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+
+import {API_URL, URL_COMICS} from './constans/api'
+import {getDataApi} from './utils/getDataApi'
+
+
+
+(async () => {
+  const data = await getDataApi.getData(API_URL + URL_COMICS);
+  console.log(data);
+})();
